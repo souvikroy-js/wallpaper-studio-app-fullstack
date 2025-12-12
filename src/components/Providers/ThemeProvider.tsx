7209902d24c7fd74c7ebe53ac5b95ca9020185f9
Souvik Roy <dev.souvikroy@gmail.com>
2025-12-12 16:30:34 +0530

    env setup complete

--- src/components/Providers/ThemeProvider.tsx
+++ src/components/Providers/ThemeProvider.tsx
@@ -2,11 +2,18 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ComponentProps } from "react";
+import ToastProvider from "./ToastProvider";
 
 type ThemeProviderProps = ComponentProps<typeof NextThemesProvider>;
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
-	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+	return (
+		<NextThemesProvider {...props}>
+			{children}
+
+			<ToastProvider />
+		</NextThemesProvider>
+	);
 };
 
 export default ThemeProvider;
--- src/lib/env/clientEnv.ts
+++ src/lib/env/clientEnv.ts
@@ -1,7 +1,13 @@
 import z from "zod";
 
-const clientEnvSchema = z.object({});
+const clientEnvSchema = z.object({
+	NEXT_PUBLIC_BETTER_AUTH_URL: z.url({
+		error: "NEXT_PUBLIC_BETTER_AUTH_URL is required",
+	}),
+});
 
-const clientEnvVars = {};
+const clientEnvVars = {
+	NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
+};
 
 export const clientEnv = clientEnvSchema.parse(clientEnvVars);
