ebbe0c130886688f8a93bf02d52ccb363b0a720f
Souvik Roy <dev.souvikroy@gmail.com>
2026-01-04 14:51:29 +0530

    Refactor category and user pages to use slugs and add metadata
    Replaced [cId] with [cSlug] for category page routing and added dynamic metadata generation for both category and user pages using Next.js's generateMetadata. Improved error handling with notFound for missing resources and updated variable names for clarity.

--- src/app/(home)/user/[uId]/page.tsx
+++ src/app/(home)/user/[uId]/page.tsx
@@ -1,14 +1,39 @@
 import WallpaperCard from "@/components/WallpaperCard";
 import prisma from "@/lib/database/dbClient";
+import { notFound } from "next/navigation";
 
-type UidPageProps = {
+type UserPageProps = {
 	params: Promise<{ uId: string }>;
 };
 
-const page = async ({ params }: UidPageProps) => {
+export const generateMetadata = async ({ params }: UserPageProps) => {
 	const { uId } = await params;
 
-	const dynamicUserWallpapers = await prisma.wallpaper.findMany({
+	try {
+		const { name } = await prisma.user.findUniqueOrThrow({
+			where: {
+				id: uId,
+			},
+			select: {
+				name: true,
+			},
+		});
+
+		return {
+			title: `${name} Wallpapers | Wallpaper App`,
+			description: `${name} Wallpapers page of Wallpaper App`,
+		};
+	} catch (error) {
+		console.error(error);
+
+		return notFound();
+	}
+};
+
+const page = async ({ params }: UserPageProps) => {
+	const { uId } = await params;
+
+	const userWallpapers = await prisma.wallpaper.findMany({
 		where: {
 			userId: uId,
 		},
@@ -21,7 +46,7 @@ const page = async ({ params }: UidPageProps) => {
 	return (
 		<>
 			<section className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-				{dynamicUserWallpapers.map((wallpaperData) => (
+				{userWallpapers.map((wallpaperData) => (
 					<WallpaperCard
 						key={wallpaperData.id}
 						wallpaper={wallpaperData}
